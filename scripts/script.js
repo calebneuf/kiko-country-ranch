56e59d5a6c4772a744b1c3cc11fd6e61eba2e230
Caleb <calebneuf@gmail.com>
2022-06-23 11:27:40 -0600

    More progress

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,23 @@
 //make top-banner-text element fade in on load
-$(document).ready(function(){
+$(document).ready(function () {
     $(".top-banner-text").fadeIn(2000);
     $(".line").fadeIn(2000);
 
-    
+
+});
+
+
+//show registration-img when registration is clicked
+$(document).ready(function () {
+    $(".registration").click(function () {
+        if ($(this).find(".registration-hidden").css('display') == 'none') {
+            $(this).find(".registration-hidden").fadeIn(300);
+            $(this).find(".temp-text").show();
+            $(this).find(".temp-text").fadeOut(5000);
+        } else {
+            $(this).find(".registration-hidden").fadeOut(500);
+            $(this).find(".temp-text").hide();
+        }
+
+    });
 });
